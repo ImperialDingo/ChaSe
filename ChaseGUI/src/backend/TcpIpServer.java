322f00c7c798ac25cc51072a3cfb3fdb246930c3
Joshua Oglesby <theimperialdingo@gmail.com>
2016-03-29 20:10:24 -0500

    Started Server management and threading

--- ChaseGUI/src/backend/TcpIpServer.java
+++ ChaseGUI/src/backend/TcpIpServer.java
@@ -2,10 +2,72 @@ package backend;
 
 import java.io.*;
 import java.net.*;
+import java.util.HashMap;
 
 
 public class TcpIpServer {
 
+	
+	public void startServer() throws Exception
+	{
+		acceptorSocket = new ServerSocket(12345);
+		listening = true;
+		new Thread()
+		{
+			public void run()
+			{
+				listenForConnections();
+			}			
+		};
+	}
+	
+	private void listenForConnections()
+	{
+		while(listening)
+		{
+			try {
+				Socket newClient = acceptorSocket.accept();
+				BufferedReader inFromClient =
+			               new BufferedReader(new InputStreamReader(newClient.getInputStream()));
+				String username = inFromClient.readLine();
+				clients.put(username, newClient);	
+
+				new Thread(){public void run(){
+					try {
+						receiveMessage(username);
+					} catch (Exception e) {
+					// TODO Auto-generated catch block
+						e.printStackTrace();
+					}}};
+			} catch (IOException e) {
+
+				e.printStackTrace();
+			}
+			
+		}
+	}
+	
+	private void routeMessage()
+	{
+		//sendMessage();
+	}
+	
+	private void receiveMessage(String username)throws Exception
+	{
+		BufferedReader inFromClient =
+	               new BufferedReader(new InputStreamReader(clients.get(username).getInputStream()));
+		while(inFromClient.ready())
+		{
+			//parse
+			//route
+		}	
+		
+	}
+	
+	private void sendMessage(String dstUsername, String message)
+	{
+		
+	}
    public static void main(String argv[]) throws Exception
       {
          String clientSentence;
@@ -25,4 +87,10 @@ public class TcpIpServer {
          }
       }
 
+   
+   private ServerSocket acceptorSocket;
+   private boolean listening;
+   private HashMap<String, Socket> clients;
+   
+   
 }
