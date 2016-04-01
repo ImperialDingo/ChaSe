62624198edd279c4de7cc1118aa666d209ce6fcc
Joshua Oglesby <theimperialdingo@gmail.com>
2016-03-31 20:26:39 -0500

    Added further threading

--- ChaseGUI/src/backend/TcpIpServer.java
+++ ChaseGUI/src/backend/TcpIpServer.java
@@ -10,35 +10,38 @@ public class TcpIpServer {
 	
 	public void startServer() throws Exception
 	{
-		acceptorSocket = new ServerSocket(12345);
+		System.out.println("Starting Server");
+		acceptorSocket = new ServerSocket(6789);
 		listening = true;
-		new Thread()
+		/*
+		Thread ServerThread = new Thread()
 		{
 			public void run()
 			{
 				listenForConnections();
 			}			
 		};
+		ServerThread.start();
+		*/
+		new Thread(()->listenForConnections()).start();
 	}
 	
 	private void listenForConnections()
 	{
+		System.out.println("Listening for connections");
 		while(listening)
 		{
+			System.out.println("Try Listening");
 			try {
+				System.out.println("Listening for connections");
 				Socket newClient = acceptorSocket.accept();
 				BufferedReader inFromClient =
 			               new BufferedReader(new InputStreamReader(newClient.getInputStream()));
 				String username = inFromClient.readLine();
 				clients.put(username, newClient);	
 
-				new Thread(){public void run(){
-					try {
-						receiveMessage(username);
-					} catch (Exception e) {
-					// TODO Auto-generated catch block
-						e.printStackTrace();
-					}}};
+				new Thread(()->receiveMessage(username)).start();
+;
 			} catch (IOException e) {
 
 				e.printStackTrace();
@@ -46,30 +49,47 @@ public class TcpIpServer {
 			
 		}
 	}
-	
-	private void routeMessage()
-	{
-		//sendMessage();
-	}
-	
-	private void receiveMessage(String username)throws Exception
-	{
-		BufferedReader inFromClient =
-	               new BufferedReader(new InputStreamReader(clients.get(username).getInputStream()));
-		while(inFromClient.ready())
-		{
-			//parse
-			//route
-		}	
-		
-	}
-	
-	private void sendMessage(String dstUsername, String message)
+
+	private void receiveMessage(String username)
 	{
 		
+		
+		try {
+			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(clients.get(username).getInputStream()));
+			
+			//String message = new String();
+			
+			while(inFromClient.ready())
+			{
+				//Get dstUsername
+				//Get message
+				//message = inFromClient.readLine();
+				//Send Message to dstUsername;
+					try {
+						sendMessage(username, username);
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+			
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
+	
+	private void sendMessage(String dstUsername, String message)throws Exception
+	{
+		DataOutputStream outToClient = new DataOutputStream(clients.get(dstUsername).getOutputStream());
+        message  = message.toUpperCase() + '\n';
+        outToClient.writeBytes(message);
+	}
+	
    public static void main(String argv[]) throws Exception
       {
+	   
+	   /*
          String clientSentence;
          String capitalizedSentence;
          ServerSocket welcomeSocket = new ServerSocket(6789);
@@ -85,6 +105,10 @@ public class TcpIpServer {
             capitalizedSentence = clientSentence.toUpperCase() + '\n';
             outToClient.writeBytes(capitalizedSentence);
          }
+         */
+	   
+	   TcpIpServer chaseServer = new TcpIpServer();
+	   chaseServer.startServer();
       }
 
    
